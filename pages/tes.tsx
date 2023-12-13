9a6135b578403efc624fe3703850b8524921d7eb
duasck <edu0017@live.com>
2023-12-12 22:22:50 -0300

    button e thumbnail
    (problema)

--- pages/tes.tsx
+++ pages/tes.tsx
@@ -1,12 +1,12 @@
 import Button from "@/app/components/Button";
 import React from 'react';
 
-const Teste: React.FC = () => {
+const tes: React.FC = () => {
     return (
         <div>
             <div>Olá mundo teste</div>
             <Button primary onClick={() => console.log('Botão primário')}>
-                <p className="text-black"> Clique aqui (Primário) </p>
+               Clique aqui (Primário)
             </Button>
             <Button secondary onClick={() => console.log('Botão secundário')}>
                 Clique aqui (Secundário)
@@ -15,4 +15,4 @@ const Teste: React.FC = () => {
     );
 };
 
-export default Teste;
+export default tes;
--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,7 +1,26 @@
-export default function Button(text: string, classTw: string) {
-    return (
-        <button className={classTw}>
-            {text}
-        </button>
-    )
+import React, { ReactNode } from 'react';
+
+interface ButtonProps {
+  children: ReactNode;
+  primary?: boolean;
+  secondary?: boolean;
+  onClick?: () => void;
 }
+
+const Button: React.FC<ButtonProps> = ({ children, primary, secondary, onClick }) => {
+  let buttonClass = 'bg-gray-300 text-black';
+
+  if (primary) {
+    buttonClass = 'bg-blue-500 text-white';
+  } else if (secondary) {
+    buttonClass = 'bg-gray-500 text-white';
+  }
+
+  return (
+    <button className={`py-2 px-4 rounded ${buttonClass}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
