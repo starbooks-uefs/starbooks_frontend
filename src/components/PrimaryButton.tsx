eff895e9888b30107aebc4fb26f5cdfde8380faa
Sergivaldo <sergivaldo123@gmail.com>
2023-12-14 21:50:53 -0300

    feat:login

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,8 @@
+type buttonProps = {
+    text:string
+    className:string | undefined
+}
+
+export default function({text,className}:buttonProps){
+    return <button className={`w-full bg-blue-500 font-semibold rounded-lg text-white px-4 py-3 ${className??""}`}>{text}</button>
+}
