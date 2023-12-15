e55fee998ae663a143ddbbec564e6ea368aaa69b
Sergivaldo <sergivaldo123@gmail.com>
2023-12-14 21:59:38 -0300

    refactor: tamanho formulário do login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,18 +11,18 @@ export default function(){
         </header>
         <main className="flex justify-between items-center max-w-6xl m-auto my-16">
             <div className="flex flex-col justify-center items-center"><img src="/undraw_reading.svg" alt="ilustration" /></div>
-            <div className="flex flex-col ">
+            <div className="flex flex-col w-full max-w-sm">
                 <div>
                     <h1 className="text-blue-400 font-semibold text-center text-3xl">Login</h1>
                     <p className="my-2 text-center">Insira os dados abaixo para acessar sua conta</p>
                 </div>
-                <form className="flex flex-col gap-4 my-8">
+                <form className="flex flex-col gap-4 mt-8">
                     <FormInput id="email" inputType="text" label="Email" placeholder="Example@email.com"/>
                     <PasswordInputLogin recoveryLink=""/>
                     <UserCategorySwitch/>
-                    <PrimaryButton className="" text="Entrar"/>    
+                    <PrimaryButton text="Entrar"/>    
                 </form>
-                <div className="flex flex-col text-center gap-3 my-4">
+                <div className="flex flex-col text-center gap-3 mt-7">
                     <p className="text-sm text-center">Ainda não é cadastrado na plataforma?</p>
                     <Link className="text-blue-500 font-medium text-sm" href={""}> Quero ser leitor</Link>
                     <Link className="text-blue-500 font-medium text-sm" href={""}> Quero ser produtor</Link>
