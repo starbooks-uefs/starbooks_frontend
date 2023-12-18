2bc45e09ad73004c7303fa91892ca772b9cd8568
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2023-12-17 23:12:34 -0300

    Conectando com o supabase

--- src/app/dashboard/author/submissions/BookForm.tsx
+++ src/app/dashboard/author/submissions/BookForm.tsx
@@ -1,7 +1,12 @@
 import FormInput from "@/components/FormInput";
+import { createClient } from "@supabase/supabase-js";
 import Multiselect from "multiselect-react-dropdown";
+import { useState } from "react";
 
 export default function BookForm() {
+    const [genderSelectList, setGenderSelectList] = useState([])
+    const [cover, setCover] = useState('')
+    const [pdf, setPdf] = useState('')
 
     const state = {
         bookGenderOptions: [
@@ -43,43 +48,95 @@ export default function BookForm() {
 
     };
 
-
-    function onSelect() {
-
+    function uploadCover(event: any) {
+        uploadFile(event, 'cover')
     }
 
-    function onRemove() {
-
+    function uploadPdf(event: any) {
+        uploadFile(event, 'pdf')
     }
 
-    function valueFormChange(value: any) {
-        console.log(value)
-    }
+
+
+    const supabase = createClient(String(process.env.NEXT_PUBLIC_SUPABASE_URL),
+        String(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY));
+
+    // Handle file upload event
+    const uploadFile = async (event: any, typeBookFile: string) => {
+        const file = event.target.files[0];
+        const bucket = String(process.env.NEXT_PUBLIC_SUPABASE_BUCKET_NAME)
+
+        // Call Storage API to upload file
+        const { data, error } = await supabase.storage
+            .from(bucket)
+            .upload(file.name, file);
+
+        // Handle error if upload failed
+        if (error) {
+            alert('Error uploading file.' + error);
+            return;
+        }
+
+        else {
+            const { data } = supabase
+                .storage
+                .from(bucket)
+                .getPublicUrl(file.name)
+
+            if (typeBookFile == 'cover') {
+                setCover(data.publicUrl)
+            }
+
+            else {
+                setPdf(data.publicUrl)
+            }
+
+            console.log(data.publicUrl)
+        }
+
+        alert('File uploaded successfully!');
+    };
+
+
 
     async function handleSubmit(event: any) {
         event.preventDefault();
 
-        const data = {
+        const newBook = {
             name: String(event.target.name.value),
-            authors: String(event.target.authors.value),
-            date: new Date(event.target.publicationDate.value)
+            author: String(event.target.author.value),
+            gender: String(genderSelectList),
+            date: '20-12-2020',
+            rating: Number(event.target.rating.value),
+            edition: Number(event.target.edition.value),
+            pages_number: Number(event.target.amountPage.value),
+            language: String(event.target.language.value),
+            publisher: String(event.target.publisher.value),
+            price: Number(event.target.price.value),
+            synopsis: String(event.target.synopsis.value),
+            cover_url: cover,
+            pdf_url: pdf,
+            id_producer: 2,
         }
 
-        console.log(data)
+        console.log(newBook)
 
         const urlBackend = process.env.NEXT_PUBLIC_URL_BACKEND
 
 
 
         try {
-            const res = await fetch(`${urlBackend}books`, {
-                method: 'GET',
+            const res = await fetch(`${urlBackend}add_book/`, {
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json"
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+
                 },
+                body: JSON.stringify(newBook)
             });
-            const data = await res.json();
-            console.log(data);
+
+
         } catch (err) {
             console.log(err);
         }
@@ -89,44 +146,41 @@ export default function BookForm() {
 
     return (<div>
 
-
-
-
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
             <FormInput id="name" inputType="text" label="Nome da Obra" placeholder="Nome" />
-            <FormInput id="authors" inputType="text" label="Autores" placeholder="Autores" />
+            <FormInput id="author" inputType="text" label="Autor" placeholder="Autor" />
             <label htmlFor="bookGenderOptions" className="font-semibold text-sm" >Gênero</label>
             <Multiselect id="gender" className="MultiSelect"
                 placeholder="Selecione"
                 isObject={false}
                 avoidHighlightFirstOption={true}
                 options={state.bookGenderOptions}
-                onSelect={onSelect}
-                onRemove={onRemove}
+                onSelect={setGenderSelectList}
+                onRemove={setGenderSelectList}
 
             />
             <FormInput id="publicationDate" inputType="date" label="Data de Publicação" placeholder="" />
 
-            <label htmlFor="indicativeClassification" className="font-semibold text-sm" >Classificação Indicativa</label>
-            <select className="bg-white border-2 rounded-lg py-4 px-3 text-sm">
-                <option value="livre">Livre</option>
-                <option value="10">10</option>
-                <option value="12">12</option>
-                <option value="14">14</option>
-                <option value="16">16</option>
-                <option value="18">18</option>
-
+            <label htmlFor="rating" className="font-semibold text-sm" >Classificação Indicativa</label>
+            <select id="rating" className="bg-white border-2 rounded-lg py-4 px-3 text-sm">
+                <option value={0}>Livre</option>
+                <option value={10}>10</option>
+                <option value={12}>12</option>
+                <option value={14}>14</option>
+                <option value={16}>16</option>
+                <option value={18}>18</option>
             </select>
 
             <FormInput id="edition" inputType="number" label="Edição" placeholder="0" />
             <FormInput id="amountPage" inputType="number" label="Quantidade de Páginas" placeholder="0" />
 
             <FormInput id="language" inputType="text" label="Idioma" placeholder="Idioma" />
-            <FormInput id="publishingCompany" inputType="text" label="Editora" placeholder="Editora" />
+            <FormInput id="publisher" inputType="text" label="Editora" placeholder="Editora" />
             <FormInput id="price" inputType="number" label="Valor" placeholder="R$ 0" />
             <label htmlFor="synopsis" className="font-semibold text-sm" >Sinopse</label>
-            <textarea className="bg-white border-2 rounded-lg py-4 px-3 text-sm" />
-            <FormInput id="pdf" inputType="file" label="Pdf do Livro" placeholder="" />
+            <textarea id="synopsis" className="bg-white border-2 rounded-lg py-4 px-3 text-sm" />
+            <FormInput id="cover" inputType="file" label="Capa do Livro" placeholder="" onValueChange={uploadCover} />
+            <FormInput id="pdf" inputType="file" label="Pdf do Livro" placeholder="" onValueChange={uploadPdf} />
 
             <div className="terms mb-8">
                 <input className="py-4 px-3 text-sm" type="checkbox" id="topping" name="topping" value="true" /> <span className="text-semibold">Li e Concordo com os Termos e Condições de Uso</span>
--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -2,12 +2,14 @@ type inputProps = {
     label: string,
     placeholder: string,
     inputType: string,
-    id: string
+    id: string,
+    onValueChange?(value: any): any;
+
 }
 
-export default function ({ label, placeholder, inputType, id }: inputProps) {
+export default function ({ label, placeholder, inputType, id, onValueChange }: inputProps) {
     return <div className="flex flex-col gap-2">
         <label htmlFor={id} className="font-semibold text-sm" >{label}</label>
-        <input className="border-2 rounded-lg py-4 px-3 text-sm" type={inputType} name={id} id={id} placeholder={placeholder} />
+        <input className="border-2 rounded-lg py-4 px-3 text-sm" type={inputType} name={id} id={id} placeholder={placeholder} onChange={onValueChange} />
     </div>
 }
