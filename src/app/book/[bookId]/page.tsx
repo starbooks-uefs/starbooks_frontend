ba85d32783598fcf7da0bf03f190b4184a1ffe1e
Ricardo Campos <rickoliver001@hotmail.com>
2024-02-11 11:56:32 -0300

    feat: Lógica de download disponível no botão

--- src/app/book/[bookId]/page.tsx
+++ src/app/book/[bookId]/page.tsx
@@ -35,6 +35,12 @@ export type Book = {
   synopsis: string
 }
 
+// Tipo Purcharse para adicionar a compra à biblioteca:
+type Pucharse = {
+  id_book: number,
+  id_reader: number
+}
+
 export default function Book ( { params }: { params: { bookId: string } } ) {
   // Ebook específico da página
   const [bookData, setBookData] = useState<Book | null>(null)
@@ -87,16 +93,55 @@ export default function Book ( { params }: { params: { bookId: string } } ) {
     }
   }, [])
 
-// Tipo Purcharse para adicionar a compra à biblioteca:
-type Pucharse = {
-  id_book: number,
-  id_reader: number
-}
+  // Estado para o texto do botão de add à biblioteca:
+  const [btnText, setBtnText] = useState("Adionar ao carrinho")
+  // Estado para alterar a estilização do botão ao ser clicado:
+  const [changeBtnClas, setchangeBtnClas] = useState('w-full bg-bg-blue font-semibold rounded-lg text-white px-4 py-3')
+  // Estado para o link de download ficar disponível:
+  const [linkDownload, setLinkDownload] = useState<string | null>(null)
+  // Estado para armazenamento das compras do leitor logado.
+  const [userPucharses, setUserPucharses] = useState<Pucharse[]>([])
 
-// Estado para o texto do botão de add à biblioteca:
-const [btnText, setBtnText] = useState("Adionar à biblioteca")
-// Estado para alterar a estilização do botão ao ser clicado:
-const [changeBtnClas, setchangeBtnClas] = useState('w-full bg-bg-blue font-semibold rounded-lg text-white px-4 py-3')
+  // Recebendo as compras já feitas pelo leitor.
+  useEffect( () => {
+    if (userToken?.user_id) {
+      const fetchPucharses = async () => {
+        try {
+          const response = await fetch(`http://127.0.0.1:8000/api/readers/${userToken.user_id}/purchases/`)
+          const data = await response.json()
+          console.log(data)
+          setUserPucharses(data)
+          
+        } catch {
+          console.error("Erro ao buscar as compras do leitor.")
+        }
+      }
+      fetchPucharses()
+      
+    }
+  }, [userToken?.user_id])
+
+  // Função que avalia se a compra do livro em visualização está na conta do usuário logado.
+  const downloadAvailable = () => {
+    console.log(userPucharses)
+    userPucharses.forEach(pucharse => {
+      console.log(pucharse)
+      if(parseInt(params.bookId) === pucharse.id_book) {
+        console.log("download disponivel")
+        setBtnText("Download")
+        setchangeBtnClas('w-full bg-green-400 font-semibold rounded-lg text-white px-4 py-3')
+        if (bookData) {
+          setLinkDownload(bookData?.pdf_url)
+        }
+      }
+    })
+  }
+
+  // Chamando a função logo depois das compras terem sido carregadas.
+  useEffect( () => {
+    downloadAvailable()
+  }, [userPucharses])
+    
 
 //Função de adicionar à biblioteca:
 const  addToLibrary = (id_reader: number, id_book: number) => {
@@ -123,14 +168,8 @@ const  addToLibrary = (id_reader: number, id_book: number) => {
     }
   }
   putBook()
-}
-// Estado para o link de download ficar disponível:
-const [linkDownload, setLinkDownload] = useState<string | null>(null)
-// Função que disponibiliza o link para fazer o download:
-const downloadBook = () => {
+  }
   
-}
-
   // Lista de 6 livros para recomendar:
   const booksCategorySix: Book[] = books.slice(0,7)
   
