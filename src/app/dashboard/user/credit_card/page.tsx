4d68b84cb644ce306bd640e4f110099439e39f15
duasck <@Dudu0017>
2024-02-14 14:36:42 -0300

    adicionando form, não completo

--- src/app/dashboard/user/credit_card/page.tsx
+++ src/app/dashboard/user/credit_card/page.tsx
@@ -5,11 +5,13 @@ const Credit_Card = ({ children }: any) => {
     return (
         <div className="flex flex-col justify-start gap-3 w-[70%]">
             <span className="p-5">Cartão de crédito </span>
-            <CreditCard cardNumber="4234 5678 9012 3455" />
-            <NoCreditCard />
+            
             <AddCreditCard/>
         </div>
     );
 };
 
 export default Credit_Card;
+
+// <CreditCard cardNumber="4234 5678 9012 3455" />
+// <NoCreditCard />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Starbooks',
   description: 'UEFS',
+  
 }
 
 export default function RootLayout({
